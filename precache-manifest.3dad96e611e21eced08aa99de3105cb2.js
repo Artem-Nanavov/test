4170ffd63c43042aa2c4b2dc18467f0509a1c409
artem_nanavov <artemnanavov@gmail.com>
2021-01-13 20:02:38 +0300

    Updates

---
+++ precache-manifest.3dad96e611e21eced08aa99de3105cb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a2d580963b272661fe549d3b64cb012",
+    "url": "/Cycle/index.html"
+  },
+  {
+    "revision": "f3a1c1f2a538a4ce24ad",
+    "url": "/Cycle/static/css/main.13dd8aa3.chunk.css"
+  },
+  {
+    "revision": "5372a322f27afef70170",
+    "url": "/Cycle/static/js/2.56cbe880.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Cycle/static/js/2.56cbe880.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3a1c1f2a538a4ce24ad",
+    "url": "/Cycle/static/js/main.fd2bd882.chunk.js"
+  },
+  {
+    "revision": "ab35632a5ed22bf5162d",
+    "url": "/Cycle/static/js/runtime-main.75919c28.js"
+  }
+]);
